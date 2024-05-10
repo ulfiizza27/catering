327899ebdfe8087a02b9f17f99ace40c3f2e2f63
ulfiizza <ulfiizza3@gmail.com>
2024-05-11 01:03:40 +0700

    [ADD] env

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIdvMOb3mTWxlbPRhHgXNsRSRRHsqKoOA",
-  authDomain: "catering-8fc58.firebaseapp.com",
-  projectId: "catering-8fc58",
-  storageBucket: "catering-8fc58.appspot.com",
-  messagingSenderId: "423794831983",
-  appId: "1:423794831983:web:451d2df9c15791a278539a",
-  measurementId: "G-NFR2HHQ9X3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
